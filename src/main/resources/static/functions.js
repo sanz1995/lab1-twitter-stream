b43317c269c81938d5f67101455c06d4a86fb739
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2018-02-15 10:54:04 +0100

    Arreglados problemas JS fix #1

--- src/main/resources/static/functions.js
+++ src/main/resources/static/functions.js
@@ -1,18 +1,19 @@
+function registerSearch() {
+    $("#search").submit(function(event){
+        event.preventDefault();
+        var target = $(this).attr('action');
+        var query = $("#q").val();
+        $.get(target, { q: query } )
+            .done( function(data) {
+                $("#resultsBlock").empty().append(data);
+            }).fail(function() {
+            $("#resultsBlock").empty();
+        });
+    });
+}
+
 $(document).ready(function() {
 	registerSearch();
 });
 
-function registerSearch() {
-	$("#search").submit(function(ev){
-		event.preventDefault();
-		target = $(this).attr('action')
-        query = $("#q").val()
-		$.get(target, { q: query } )
-            .done( function(data) {
-	    		$("#resultsBlock").empty().append(data);
-            }).fail(function() {
-                $("#resultsBlock").empty();
-            });
-	});
-}
 
